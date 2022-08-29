6e34938db8a0d8538819d0b9f70ac055042e98dd
Hosein <h.eftekhar.m@gmail.com>
2022-08-29 23:55:30 +0430

    remove redundant codes

--- index.js
+++ index.js
@@ -15,12 +15,7 @@ app.use(async (req,res,next)=>{
   next();
 });
 
-app.get('/wall', function (req, res) {//returns all messages
-  let messages=getMessagesFromDb();
-  res.json(messages);
-});
-
-app.post('/wall/register', async function(req,res){// writes a new message submitted by a user
+app.post('/api/v1/register', async function(req,res){// registers a user
   let email = req.body.email;
   await registerUser(email);
   res.json({
@@ -28,83 +23,9 @@ app.post('/wall/register', async function(req,res){// writes a new message submi
   })
 });
 
-app.get('/wall/:username',function (req,res){//returns messages written by a user
-  let username = req.params.username;
-  let userMessages=getMessagesFromDb(username);
-  let response={};
-  response[username]=userMessages;
-  res.json(response);
-});
-
-app.post('/wall/:username',function(req,res){// writes a new message submitted by a user
-  let username = req.params.username;
-  let message = req.body.msg;
-  writeMessageToDb(username,message);
-  res.json({
-    "status":"Your message was written on the databse"   
-  })
-});
-
-app.delete('/wall/:username',function(req,res){// deletes a user messages
- 
-  let jsonDatabase=readDatabaseAsJson();  
-  
-  jsonDatabase[username]=[];
-  
-  fs.writeFileSync('database.json', JSON.stringify(jsonDatabase));
- 
-  res.json({
-    "status":"Your message was written on the databse"   
-  })
-
-});
-
 app.listen(port, () => {
   console.log(`DiaryOnWall app listening on port ${port}`)
 });
-
-
-function writeMessageToDb(username,message){//writes a message to the database
-  let jsonDatabase=readDatabaseAsJson();
-  
-  let userMessages=jsonDatabase[username];
-
-  if(userMessages == null){
-    userMessages=[];   
-  }
-  
-  userMessages.push(message);
-  
-  jsonDatabase[username]=userMessages;
-  
-  fs.writeFileSync('database.json', JSON.stringify(jsonDatabase));
-}
-
-function getMessagesFromDb(username){// get messages from the database 
-  let jsonDatabase=readDatabaseAsJson();
-
-  if(username==null) return jsonDatabase;
-  
-  let userMessages=jsonDatabase[username];
-
-  if(userMessages == null){
-    userMessages=[];   
-  }
-
-  return userMessages;
-  
-}
-
-function readDatabaseAsJson(){
-  let jsonDatabase = null;
-  try{
-    let rawDatabase = fs.readFileSync('database.json');
-    jsonDatabase = JSON.parse(rawDatabase);  
-  }catch(e){
-    jsonDatabase={};
-  }
-  return jsonDatabase;
-}
 
 let db;
 
@@ -117,7 +38,6 @@ async function connectDb(){
 async function registerUser(email){
   console.log("registering "+ email);
   const collection = db.collection('users');
-
   collection.insertOne({
     "email":email
   });
